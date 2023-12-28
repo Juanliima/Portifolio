61b4717df048b59d3893edce8ace45b2f3f56f54
unknown <juanpabloblima@gmail.com>
2023-12-28 19:28:08 -0300

    nomes ,add de pastas, favicon

--- Horario-do-dia/horario.js
+++ Horario-do-dia/horario.js
@@ -9,13 +9,13 @@ var hora= data.getHours()
 msg.innerHTML = `Agora são ${hora} horas!`
 
 if(hora >=0 && hora < 12){
-    img.src= 'manha.png'
+    img.src= 'midia/manha.png'
 document.body.style.background ='#F2C84B'
 }else if (hora >= 12 && hora <= 18){
-    img.src ='tarde.png'
+    img.src ='midia/tarde.png'
     document.body.style.background ='#BDCFDD'
 }else {
-    img.src ='noite.png.png'
+    img.src ='midia/noite.png.png'
     document.body.style.background ='#162730'
     
 }
--- Verificador-Idade/calculo.js
+++ Verificador-Idade/calculo.js
@@ -16,31 +16,31 @@ if (fano.value.length == 0 || Number(fano.value) > ano) {
    if (fsex[0].checked){
     genero = 'homem'
     if(idade >=0 && idade< 10){//criança
-        img.setAttribute('src','foto-bebe-h.png')
+        img.setAttribute('src','midia/foto-bebe-h.png')
     }else if (idade < 30){ //jovem
-        img.setAttribute('src','jovem-h.png')
-    }else if(idade < 50){
-        img.setAttribute('src','homem.png')
+        img.setAttribute('src','midia/jovem-h.png')
+    }else if(idade < 50){//homem
+        img.setAttribute('src','midia/homem.png')
     }else{
         //idoso
-        img.setAttribute('src','idoso.png')
+        img.setAttribute('src','midia/idoso.png')
     }
 
    }else if(fsex[1].checked){
     genero= 'mulher'
 
-    if(idade >=0 && idade< 10){//criança
-        img.setAttribute('src','bebe-m.png')
+    if(idade >=0 && idade< 10){//criança-menina
+        img.setAttribute('src','midia/bebe-m.png')
 
-    }else if (idade < 30){ //jovem
-        img.setAttribute('src','jovem-m.png')
+    }else if (idade < 30){ //jovem-mulher
+        img.setAttribute('src','midia/jovem-m.png')
 
-    }else if(idade < 50){
-        img.setAttribute('src','mulher.png')
+    }else if(idade < 50){// mulher
+        img.setAttribute('src','midia/mulher.png')
 
     }else{
-        //idoso
-        img.setAttribute('src','idosa.png')
+        //idosa
+        img.setAttribute('src','midia/idosa.png')
     }
 
    }
